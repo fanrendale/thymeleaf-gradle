ed440156284df492e40ede5b4022537993484e3d
xjf <1053314919@qq.com>
2019-01-28 23:17:01 +0800

    实体需自己写方法

--- src/main/java/com/xjf/springboot/domain/User.java
+++ src/main/java/com/xjf/springboot/domain/User.java
@@ -8,7 +8,10 @@ import lombok.Data;
  */
 @Data
 public class User {
-    private Long id;        //实体的唯一标识
+    /**
+     * 实体的唯一标识
+     */
+    private Long id;
 
     private String name;
 
@@ -20,6 +23,30 @@ public class User {
     public User(Long id, String name, String email) {
         this.id = id;
         this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 }
